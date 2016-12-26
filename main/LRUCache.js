e055a4999b6a9e3965d6e7416307424259c09383
mofthecross <mofthecross@gmail.com>
2016-12-25 19:05:50 -0800

    Implement LRUCache with Hashmap and DoublyLinkedList

--- main/LRUCache.js
+++ main/LRUCache.js
@@ -9,3 +9,39 @@ set(key, value) - Set or insert the value if the key is not already present.
 When the cache reached its capacity, it should invalidate the least recently
 used item before inserting a new item.
 */
+
+const DoublyLinkedList = require('es6datastructures').DoublyLinkedList;
+
+class LRUCache {
+  constructor(limit) {
+    this.storage = {};
+    this.list = new DoublyLinkedList();
+    this.limit = limit;
+  }
+
+  get(key) {
+    if (key in this.storage) {
+      let data = this.storage[key].data;
+      this.set(data[0], data[1]);
+      return data[1];
+    } else {
+      return -1;
+    }
+  }
+
+  set(key, value) {
+    if (key in this.storage) {
+      let node = this.storage[key];
+      this.list.remove(node);
+    }
+
+    if (this.limit === this.list.size()) {
+      let leastRecentlyUsed = this.list.head();
+      this.list.remove(leastRecentlyUsed);
+      delete this.storage[leastRecentlyUsed.data[0]];
+    }
+    this.storage[key] = this.list.append([key, value]);
+  }
+}
+
+module.exports = LRUCache;
